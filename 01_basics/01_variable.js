a5ff5c566072541ff365a09aca6cfb5320695393
Arshad Qureshi <66543017+Arshadak04@users.noreply.github.com>
2024-01-11 12:54:19 +0000

    New

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,31 @@
+const a=10
+let b=20
+var c=30
+
+console.table([a,b,c])
+
+// a=40  we can not change const variables 
+b=50
+c=60
+
+console.table([a,b,c])
+
+
+// Datatypes in js
+
+// integer 
+
+let number =10
+
+//string type 
+
+let string = "Arshad Qureshi"
+
+console.table([number , string])
+ // null and undefined
+
+ let nulltype=null
+let unde;
+
+console.log(typeof nulltype)
+console.log(typeof unde)

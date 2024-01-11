65b5d3edd9d03aecab5b589dfb8de663890c7dba
Arshad Qureshi <66543017+Arshadak04@users.noreply.github.com>
2024-01-11 13:14:15 +0000

    Conversion

---
+++ 01_basics/02conversion.js
@@ -0,0 +1,30 @@
+// let score="22"
+// let score="22a"
+// let score=22
+// let score=null
+// let score=undefined
+// let score=true
+// let score=Nan
+
+
+// let change=Number(score)
+
+// console.log(typeof score)
+// console.log(typeof change)
+// console.log(score)
+// console.log(change)
+
+// let bool=1;
+let bool="Arshad";
+
+let boolc=Boolean(bool)
+
+console.log(typeof bool)
+console.log(typeof boolc)
+console.log(bool)
+console.log(boolc)
+
+// when bool = 1 =true
+// when bool = 0 =false
+// when bool = "" =false
+// when bool = "Arshad" =true
